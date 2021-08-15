3d7a626b0c723dd951e1c49db4daa66288a93253
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-15 16:44:41 +0200

    a

--- applications/app.cpp
+++ applications/app.cpp
@@ -1,10 +1,9 @@
-using namespace std;
-
-
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 
 int main (int, char**)
 {
+    auto instance = VkInstance {};
 	return 0;
 }
